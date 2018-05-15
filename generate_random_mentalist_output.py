c5522db44c8bbc0d815d255e1d02c01547c5ccc4
Dan Fornika <dfornika@gmail.com>
2018-05-15 12:03:32 -0700

    Added script for generating random MLST calls

---
+++ generate_random_mentalist_output.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import sys
+from random import randint
+
+sample_ids = ["sample-" + str(id) for id in range(0, int(sys.argv[1]))]
+locus_ids = ["locus-"+ str(id) for id in range(0, int(sys.argv[2]))]
+
+sequence_types = {}
+
+for sample in sample_ids:
+    sequence_types[sample] = []
+    for j, locus in enumerate(locus_ids):
+        sequence_types[sample].append(randint(0, 9))
+
+print('\t', '\t'.join(locus_ids), sep='')
+
+for sample in sequence_types:
+    print(sample, '\t'.join(map(str, sequence_types[sample])), sep='\t')
