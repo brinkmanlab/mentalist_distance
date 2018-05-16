40df2f1147155bfc689970e7320553be32dcffe2
Dan Fornika <dfornika@gmail.com>
2018-05-15 20:24:48 -0700

    Added one more test

--- tests/test_mentalist_distance.R
+++ tests/test_mentalist_distance.R
@@ -1,4 +1,9 @@
-test_that("Test ham_distance", {
-  X <- data.frame(c(0), c(0))
-  expect_equal(ham_distance(X), as.matrix(0))
+test_that("Test ham_distance 0", {
+  X <- data.frame(c(0,0))
+  expect_equal(ham_distance(X), matrix(c(0,0,0,0), nrow=2, ncol=2))
+})
+
+test_that("Test ham_distance 1", {
+  X <- data.frame(c(0,1))
+  expect_equal(ham_distance(X), matrix(c(0,1,1,0), nrow=2, ncol=2))
 })
